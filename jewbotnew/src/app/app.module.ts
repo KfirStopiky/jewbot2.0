46f0e8297fefb475d21cdcbcaae7a05cc4debb39
Kfir <stopiky2@gmail.com>
2021-09-21 21:58:41 +0300

    styling

--- jewbotnew/src/app/app.module.ts
+++ jewbotnew/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { SwapComponent } from './Components/swap/swap.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SwapComponent
   ],
   imports: [
     BrowserModule,
