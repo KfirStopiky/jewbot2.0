4b705eb7e8f82a4a05bc51c88c4eccd3a4a07281
Kfir <stopiky2@gmail.com>
2021-09-22 15:36:16 +0300

    modal

---
+++ jewbotnew/src/app/Components/transaction-settings/transaction-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-settings',
+  templateUrl: './transaction-settings.component.html',
+  styleUrls: ['./transaction-settings.component.css']
+})
+export class TransactionSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
