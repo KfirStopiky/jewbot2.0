96966aa3ba7e295bccc256deabba9476ed9a2576
Kfir <stopiky2@gmail.com>
2021-09-28 14:16:19 +0300

    transaction settings modal

--- jewbotnew/src/app/Components/swap/swap.component.ts
+++ jewbotnew/src/app/Components/swap/swap.component.ts
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwapComponent implements OnInit {
   showmodal: boolean = false;
+  slipaggeTolerance: any = 0;
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.slipaggeTolerance = this.slipaggeTolerance;
+  }
   openSettingsModal() {
     this.showmodal = !this.showmodal;
   }
   closeSettingsModal() {
     this.showmodal = !this.showmodal;
+  }
+  onKey(event: any) {
+    this.slipaggeTolerance = event.target.value;
   }
 }
